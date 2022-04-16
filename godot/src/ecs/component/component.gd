b25a7056c235d12452f5b136031cdb16f768f26c
victrolaface <rlovelett@gmail.com>
2022-04-15 19:05:20 -0500

    component_mgr created

--- godot/src/ecs/component/component.gd
+++ godot/src/ecs/component/component.gd
@@ -1,8 +1,9 @@
 class_name Component extends Resource
 
 # signals
-signal received_parent_entity
-signal received_parent_entity_id
+signal received_entity
+signal received_entity_id
+signal is_named
 signal is_initialized
 signal is_enabled
 signal is_disabled
@@ -10,29 +11,27 @@ signal is_destroyed
 
 # properties
 export(String) var name setget , get_name
-export(Resource) var parent_entity setget , get_parent_entity
-export(int) var parent_entity_id setget , get_parent_entity_id
+export(Resource) var entity setget , get_parent_entity
+export(int) var entity_id setget , get_parent_entity_id
 export(bool) var initialized setget , get_initialized
 export(bool) var enabled setget , get_enabled
 export(bool) var destroyed setget , get_destroyed
 
 # fields
 var cmpnt_data: Dictionary
+const COMPONENT_TYPE = "Component"
+
 
 # default methods
 func _init(_ref_entity = null, _entity_id = 0, _name = "", _ref_self = null):
 	make_local()
-	var init_connect = SignalUtility.is_self_connect_valid("is_initialized", self, "_on_initialized", CONNECT_ONESHOT)
-	var enable_connect = SignalUtility.is_self_connect_valid("is_enabled", self, "_on_enabled", CONNECT_DEFERRED)
-	var disable_connect = SignalUtility.is_self_connect_valid("is_disabled", self, "_on_disabled", CONNECT_DEFERRED)
-	var destroy_connect = SignalUtility.is_self_connect_valid("is_destroyed", self, "_on_destroyed", CONNECT_ONESHOT)
-	#
-	var entity_connected = SignalUtility.is_self_connect_valid(
-		"received_parent_entity", self, "_on_received_parent_entity", CONNECT_ONESHOT
-	)
-	var entity_id_connected = SignalUtility.is_self_connect_valid(
-		"received_parent_entity_id", self, "_on_received_entity_id", CONNECT_ONESHOT
-	)
+	var ent = SignalUtility.is_self_connect_valid("received_entity", self, "_on_received_entity", CONNECT_ONESHOT)
+	var ent_id = SignalUtility.is_self_connect_valid("received_entity_id", self, "_on_received_entity_id", CONNECT_ONESHOT)
+	var nmed = SignalUtility.is_self_connect_valid("is_named", self, "_on_named", CONNECT_ONESHOT)
+	var init = SignalUtility.is_self_connect_valid("is_initialized", self, "_on_initialized", CONNECT_ONESHOT)
+	var enbl = SignalUtility.is_self_connect_valid("is_enabled", self, "_on_enabled", CONNECT_DEFERRED)
+	var dsbl = SignalUtility.is_self_connect_valid("is_disabled", self, "_on_disabled", CONNECT_DEFERRED)
+	var dstr = SignalUtility.is_self_connect_valid("is_destroyed", self, "_on_destroyed", CONNECT_ONESHOT)
 	cmpnt_data = {
 		"name": _name,
 		"ref_self": _ref_self,
@@ -40,14 +39,17 @@ func _init(_ref_entity = null, _entity_id = 0, _name = "", _ref_self = null):
 		"entity_id": _entity_id,
 		"state":
 		{
-			"initialized_connected": init_connect,
-			"enabled_connected": enable_connect,
-			"disabled_connected": disable_connect,
-			"destroyed_connected": destroy_connect,
-			"received_parent_entity_connected": entity_connected,
-			"received_parent_entity_id_connected": entity_id_connected,
-			"has_parent_entity": false,
-			"has_parent_entity_id": false,
+			"received_entity_connected": ent,
+			"received_entity_id_connected": ent_id,
+			"named_connected": nmed,
+			"initialized_connected": init,
+			"enabled_connected": enbl,
+			"disabled_connected": dsbl,
+			"destroyed_connected": dstr,
+			"has_name": false,
+			"has_ref_self": false,
+			"has_entity": false,
+			"has_entity_id": false,
 			"initialized": false,
 			"enabled": false,
 			"destroyed": false
@@ -58,18 +60,20 @@ func _init(_ref_entity = null, _entity_id = 0, _name = "", _ref_self = null):
 		&& ObjectUtility.is_valid(cmpnt_data.ref_self)
 		&& ObjectUtility.is_valid(cmpnt_data.ref_entity)
 		&& EntityUtility.is_id_valid(cmpnt_data.entity_id)
+		&& cmpnt_data.state.received_entity_connected
+		&& cmpnt_data.state.received_entity_id_connected
+		&& cmpnt_data.state.named_connected
 		&& cmpnt_data.state.initialized_connected
 		&& cmpnt_data.state.enabled_connected
 		&& cmpnt_data.state.disabled_connected
 		&& cmpnt_data.state.destroyed_connected
-		&& cmpnt_data.state.received_parent_entity_connected
-		&& cmpnt_data.state.received_parent_entity_id_connected
 	)
 	if valid_init:
-		emit_signal("received_parent_entity")
-		emit_signal("received_parent_entity_id")
-		if cmpnt_data.state.has_parent_entity && cmpnt_data.state.has_parent_entity_id:
-			emit_signal("is_initialized")
+		emit_signal("received_entity")
+		emit_signal("received_entity_id")
+		emit_signal("is_named")
+		emit_signal("is_initialized")
+
 
 # helper methods
 func _can_enable(_enabled: bool):
@@ -85,7 +89,12 @@ func _on_received_parent_entity():
 func _on_received_entity_id():
 	cmpnt_data.state.has_entity_id = true
 	cmpnt_data.state.received_parent_entity_id_connected = false
-#
+
+
+func _on_named():
+	cmpnt_data.state.has_name = true
+	cmpnt_data.state.named_connected = false
+
 
 func _on_initialized():
 	cmpnt_data.state.initialized = true
@@ -103,16 +112,23 @@ func _on_disabled():
 
 func _on_destroyed():
 	cmpnt_data.state.destroyed = true
+	cmpnt_data.state.destroyed_connected = false
 
 
 # setters, getters functions
+func get_type():
+	return COMPONENT_TYPE
+
+
 func get_parent_entity():
 	if cmpnt_data.state.has_parent_entity:
 		return cmpnt_data.ref_entity
 
+
 func get_parent_entity_id():
 	if cmpnt_data.state.has_parent_entity_id:
 		return cmpnt_data.entity_id
+
 
 func get_name():
 	if cmpnt_data.state.initialized:
@@ -130,10 +146,12 @@ func get_enabled():
 func get_destroyed():
 	return cmpnt_data.state.destroyed
 
+
 # public methods
 func make_local():
 	if not resource_local_to_scene:
 		resource_local_to_scene = true
+
 
 func enable():
 	if _can_enable(true):
@@ -145,6 +163,10 @@ func disable():
 		emit_signal("is_disabled")
 
 
-func _destroy():#_do: bool):
+func _destroy():  #_do: bool):
 	disable()
 	emit_signal("is_destroyed")
+
+
+func is_type(_type):
+	return _type == COMPONENT_TYPE or .is_type(_type)
--- godot/src/util/component_utility.gd
+++ godot/src/util/component_utility.gd
@@ -2,4 +2,8 @@ class_name ComponentUtility
 
 
 static func is_valid(_component = null):
-	return not _component == null && _component.is_type("Component")
+	return (
+		not _component == null
+		&& _component.is_type(Component.COMPONENT_TYPE)
+		&& _component.get_type() == Component.COMPONENT_TYPE
+	)
