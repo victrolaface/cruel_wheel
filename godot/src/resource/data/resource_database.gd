e8cb4184a573954bfb55ed16afb02ecef3cfab33
victrolaface <rlovelett@gmail.com>
2022-04-24 23:16:25 -0500

    resource/singleton db refactor

--- godot/src/resource/data/resource_database.gd
+++ godot/src/resource/data/resource_database.gd
@@ -34,9 +34,12 @@ var _data = {
 		"has_db": false,
 		"cached": false,
 		"initialized": false,
-		"enabled": false,
+		"saved": false,
+		"enabled": false
 	}
 }
+
+const _EN_DB_ERROR = "cannot enable database."
 
 
 # public inherited methods
@@ -75,19 +78,186 @@ func _init(_self_ref = null, _manager = null):
 		_data.state.has_db_path = _set_has_db_path(_data.db_path)
 	if _can_load_db():
 		var loaded = ResourceLoader.load(_data.db_path)
-		if loaded == null or loaded.first_initialization:
+		var class_names = ClassType.from_name(_data.item_type).get_inheritors_list()
+		var class_names_amt = class_names.size()
+		var has_class_names = class_names_amt > 0
+		var first_init = loaded.first_initialization
+		if loaded == null or first_init:
 			_data.db = ClassType.from_name(_data.table_type)
-			var is_db = not _data.db == null && _data.db.get_class() == _data.table_type
+			_data.db._init()
+			var is_db = not _data.db == null
+			if first_init && _data.db.init_from_manager(_data.self_ref, _data.manager_ref):
+				is_db = is_db && _data.db.enabled && _data.db.get_class() == _data.table_type
 			var classes = [_data.table_type, _data.base_class_names[0], _data.base_class_names[1]]
 			var is_class = false
 			for c in classes:
 				is_class = _data.db.is_class(c)
 				if not is_class:
 					break
-			if is_db && is_class:
-				# can init db
-				# validate
-				pass
+			if has_class_names:
+				if is_db:
+					if is_class:
+						var added_valid = _init_added_valid(class_names_amt, class_names)
+						_data.state.has_db = _data.db.has_items && added_valid
+						_init_on_has_db(first_init)
+					else:
+						_on_warning("database not of class type.", true)
+				else:
+					_on_cannot_enable_warning(true)
+			else:
+				_on_warning("cannot find items of class type for database.", true)
+		else:
+			_data.db = loaded.db
+			if _data.db.enable(_data.self_ref, _data.manager_ref):
+				_init_validate(false, false, has_class_names, class_names)
+			else:
+				_on_cannot_enable_warning()
+				if _data.db.remove_disabled():
+					_init_validate(false, false, has_class_names, class_names)
+				else:
+					_on_warning("cannot remove disabled items from database.", false)
+					_init_reset_db(false, has_class_names, class_names)
+
+
+# public methods
+func disable():
+	if _data.state.enabled:
+		if _data.db.disable():
+			_data.manager_ref = null
+			_data.self_ref = null
+			_data.state.has_db_path = false
+			_data.state.has_name = false
+			_data.state.has_self_ref = false
+			_data.state.has_base_class_names = false
+			_data.state.has_item_type = false
+			_data.state.has_table_type = false
+			_data.state.has_db = false
+			_data.state.cached = false
+			_data.state.initialized = false
+			_data.state.enabled = false
+			_data.state.saved = true
+			if ResourceSaver.save(_data.db_path, self):
+				_data.state.saved = false
+			else:
+				_on_cannot_save_warning()
+		else:
+			_on_warning("cannot disable database.", false)
+	return not _data.state.enabled
+
+
+# private helper methods
+func _on_init(_push_err = false, _has_class_names = false, _class_names = null):
+	if _push_err:
+		push_error(_EN_DB_ERROR)
+	if _has_class_names:
+		var added_valid = false
+		var proc_added = false
+		var rem_valid = false
+		var proc_rem = false
+		if not _data.db.has_keys(_class_names):
+			var idx_to_keep = []
+			var idx = 0
+			for n in _class_names:
+				if not _data.db.has_key(n):
+					idx_to_keep.append(idx)
+				idx = idx + 1
+			if idx_to_keep.count() > 0:
+				var names_to_keep = []
+				for i in idx_to_keep:
+					names_to_keep.append(_class_names[i])
+				if names_to_keep.count() > 0:
+					_class_names = PoolStringArray(names_to_keep)
+					var class_names_amt = _class_names.size()
+					if class_names_amt > 0:
+						added_valid = _init_added_valid(class_names_amt, _class_names)
+						if not added_valid:
+							_on_added_invalid_warning()
+						proc_added = true
+		if _data.db.has_keys_sans(_class_names):
+			var init_amt = _data.db.items_amount
+			var keys_sans = _data.db.keys_sans(_class_names)
+			var keys_sans_amt = keys_sans.size()
+			if keys_sans_amt > 0 && _data.db.remove_keys(keys_sans):
+				var rem_amt = init_amt - keys_sans_amt
+				rem_valid = _data.db.items_amount == rem_amt
+				proc_rem = true
+		var proc_added_valid = proc_added && added_valid
+		var proc_rem_valid = proc_rem && rem_valid
+		var proc_added_rem_valid = proc_added_valid && proc_rem_valid
+		var sans_rem_valid = not proc_rem && not rem_valid
+		var sans_added_valid = not proc_added && not added_valid
+		var init_valid = sans_added_valid && sans_rem_valid
+		var added_sans_rem_valid = proc_added_valid && sans_rem_valid
+		var rem_sans_added_valid = proc_rem_valid && sans_added_valid
+		var db_valid = init_valid or proc_added_rem_valid or added_sans_rem_valid or rem_sans_added_valid
+		_data.state.has_db = _data.db.has_items && db_valid
+		_init_on_has_db()
+
+
+func _init_added_valid(_to_add_amt = 0, _class_names = null):
+	var added_valid = false
+	var init_amt = _data.db.items_amount
+	var added_amt = 0
+	for n in _class_names:
+		var i = ClassType.from_name(n)
+		i._init()
+		if i.init_from_manager(_data.self_ref, _data.manager_ref):
+			if i.enabled && _data.db.add(i.name, i):
+				added_amt = added_amt + 1
+			else:
+				_on_warning("cannot add item to database.", false)
+	var init_to_add_amt = init_amt + _to_add_amt
+	var init_added_amt = init_amt + added_amt
+	var amts = [init_to_add_amt, init_added_amt, added_amt]
+	for amt in amts:
+		added_valid = _data.db.items_amount == amt
+		if not added_valid:
+			_on_warning("cannot validate amount of added items to database.", false)
+			break
+	return added_valid
+
+
+func _init_validate(_from_rem_invalid = false, _from_rem_all = false, _has_class_names = false, _class_names = null):
+	if _data.db.validate():
+		_on_init(false, _has_class_names, _class_names)
+	else:
+		_on_warning("cannot validate database.", false)
+		if _from_rem_all:
+			_on_init(_from_rem_all)
+		elif _from_rem_invalid:
+			_init_reset_db(_from_rem_invalid, _has_class_names, _class_names)
+		if _data.db.remove_invalid():
+			_from_rem_invalid = true
+			_init_validate(_from_rem_invalid, false, _has_class_names, _class_names)
+		else:
+			_on_warning("cannot remove invalid items from database.", false)
+			_init_reset_db(true, _has_class_names, _class_names)
+
+
+func _init_reset_db(_reset_db = false, _has_class_names = false, _class_names = null):
+	if not _reset_db:
+		_reset_db = not _has_class_names && _data.db.items_amount > 0
+	if _reset_db:
+		if _data.db.remove_all():
+			_init_validate(false, true, _has_class_names, _class_names)
+		else:
+			_on_warning("cannot remove all invalid items from database.", true)
+
+
+func _init_on_has_db(_first_init = false):
+	if _data.state.has_db:
+		_data.state.first_init = not _first_init if _first_init else _first_init
+		_data.state.cached = _data.state.has_db
+		_data.state.initialized = _data.state.cached
+		_data.state.enabled = _data.state.initialized
+		if _data.state.enabled:
+			_data.state.saved = ResourceSaver.save(_data.path, self)
+			if not _data.state.saved:
+				_on_cannot_save_warning(true)
+		else:
+			_on_cannot_enable_warning(true)
+	else:
+		_on_added_invalid_warning(true)
 
 
 func _can_load_db():
@@ -103,44 +273,6 @@ func _can_load_db():
 	)
 
 
-func _set_cached():
-	_data.state.cached = _can_load_db() && _data.state.has_db
-
-
-"""
-var data = ResourceLoader.load(_PATH)
-var db_valid = false
-var class_names = ClassType.from_name(_BASE_CLASS_NAME).get_inheritors_list()
-var class_names_amt = class_names.count()
-var has_class_names = class_names_amt > 0
-var first_init = data.first_initialization
-if first_init:
-    _on_init_name_mgr(self, _manager, first_init)
-    if has_class_names:
-        var db_init = SingletonTable.new(_CLASS_NAME, self, _manager)
-        if db_init.enabled:
-            var added = _on_add_init(class_names, _manager, db_init)
-            db_init = added.db
-            var items_is_cn_amt = _is_amt(db_init.items_amount, class_names_amt)
-            var items_is_added_amt = _is_amt(db_init.items_amount, added.amount)
-            db_valid = db_init.has_items && items_is_cn_amt && items_is_added_amt
-            _on_db_valid(db_valid, db_init, true)
-
-func _on_db_valid(_is_valid, _db, _is_first_init):
-if _is_valid:
-    _data.db = _db
-    _data.state.has_db = true
-    _data.state.cached = _data.state.has_name && _data.state.has_self_ref && _data.state.has_manager_ref
-    _data.state.initialized = _data.state.cached
-    _data.state.enabled = _data.state.initialized
-    if _is_first_init:
-        _data.state.first_init = not _data.state.initialized
-    if _data.state.enabled:
-        ResourceLoader.save(_PATH, self)
-"""
-
-
-# private helper methods
 func _on_init_has(_has_item = false, _self_has_item = false, _item = null):
 	var item = null
 	if not _has_item && _self_has_item:
@@ -158,6 +290,24 @@ func _set_has_table_type(_table_type = ""):
 
 func _set_has_db_path(_db_path = ""):
 	_data.state.has_db_path = PathUtility.is_valid(_db_path)
+
+
+func _on_warning(_message = "", _push_err = true):
+	push_warning(_message)
+	if _push_err:
+		_on_init(_push_err)
+
+
+func _on_cannot_save_warning(_push_err = false):
+	_on_warning("cannot save database", _push_err)
+
+
+func _on_cannot_enable_warning(_push_err = false):
+	_on_warning(_EN_DB_ERROR, _push_err)
+
+
+func _on_added_invalid_warning(_push_err = false):
+	_on_warning("cannot validate added items of class type to database.", _push_err)
 
 
 # setters, getters functions
@@ -205,116 +355,3 @@ func get_db_path():
 
 func get_has_db_path():
 	return _data.state.has_db_path
-
-	"""        
-        else:
-			_on_init_name_mgr(self, _manager, first_init)
-			if not data.db.enable(self, _manager):
-				if not data.db.remove_disabled(_manager):
-					push_error("unable to remove disabled items.")
-			if not data.db.validate():  #
-				if not data.db.remove_invalid():
-					push_error("unable to remove invalid items.")
-			if not has_class_names && data.db.items_amount > 0:
-				if not data.db.remove_all():
-					push_error("unable to remove all items")
-			if has_class_names:
-				if not data.db.has_keys(class_names):
-					var idx_to_keep = []
-					var idx = 0
-					for cn in class_names:
-						if not data.db.has_key(cn):
-							idx_to_keep.append(idx)
-						idx = idx + 1
-					if idx_to_keep.count() > 0:
-						var names_to_keep = []
-						for i in idx_to_keep:
-							names_to_keep.append(class_names[i])
-						if names_to_keep.count() > 0:
-							class_names = PoolStringArray(names_to_keep)
-							class_names_amt = class_names.size()
-							if class_names_amt > 0:
-								var init_amt = data.db.items_amount
-								var added = _on_add_init(class_names, _manager, data.db)
-								data.db = added.db
-								var init_classes_amt = init_amt + class_names_amt
-								var init_added_amt = init_amt + added.amount
-								var is_init_classes_amt = _is_amt(data.db.items_amount, init_classes_amt)
-								var is_init_added_amt = _is_amt(data.db.items_amount, init_added_amt)
-								db_valid = is_init_classes_amt && is_init_added_amt
-								if not db_valid:
-									push_error("unable to add items.")
-				if data.db.has_keys_sans(class_names):
-					var init_amt = data.db.items_amount
-					var keys_sans = data.db.keys_sans(class_names)
-					var keys_sans_amt = keys_sans.size()
-					if keys_sans_amt > 0 && data.db.remove_keys(keys_sans):
-						var rem_amt = init_amt - keys_sans_amt
-						var is_init_rem_amt = _is_amt(data.db.items_amount, rem_amt)
-						if db_valid:
-							db_valid = db_valid && is_init_rem_amt
-						else:
-							db_valid = is_init_rem_amt
-						if not db_valid:
-							push_error("unable to remove invalid items.")
-				_on_db_valid(db_valid, data.db, false)
-
-
-# public methods
-func disable():
-	if _data.state.enabled:
-		if _data.db.disable():
-			_data.manager_ref = null
-			_data.self_ref = null
-			_data.state.first_init = false
-			_data.state.has_manager_ref = false
-			_data.state.has_self_ref = false
-			_data.state.cached = false
-			_data.state.initialized = false
-			_data.state.enabled = false
-			if not ResourceSaver.save(_PATH, self):
-				push_warning("unable to save database.")
-		else:
-			push_warning("unable to disable to disable database.")
-	return not _data.state.enabled
-
-
-# private helper methods
-func _on_init_name_mgr(_self_ref, _mgr, _is_first_init):
-	_data.name = _CLASS_NAME
-	_data.state.has_name = StringUtility.is_valid(_data.name)
-	_data.self_ref = self
-	_data.state.has_self_ref = not _data.self_ref == null
-	_data.manager_ref = _mgr
-	_data.state.has_manager_ref = not _data.manager_ref == null
-	if _is_first_init:
-		_data.state.first_init = false
-
-
-func _is_amt(_amt1, _amt2):
-	return _amt1 == _amt2
-
-
-func _on_add_init(_class_names, _manager, _db):
-	var added_amt = 0
-	for cn in _class_names:
-		var s = ClassType.from_name(cn)
-		s._init()
-		if s.init_from_manager(_manager):
-			if s.enabled && _db.add(s.name, s):
-				added_amt = added_amt + 1
-	var added = {"amount": added_amt, "db": _db}
-	return added
-
-
-
-
-
-# setters, getters functions
-func get_enabled():
-	return _data.state.enabled
-
-
-func get_first_initialization():
-	return _data.state.first_init
-        """
